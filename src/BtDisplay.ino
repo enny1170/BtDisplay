49b5787fa1fd04c0a3adb1bc7d3d8cd7ebbc38e0
jroth-haj <jroth.de@gmail.com>
2018-08-10 23:48:29 +0200

    first Checkin

--- src/BtDisplay.ino
+++ src/BtDisplay.ino
@@ -1,11 +1,77 @@
+#include <wifi.h>
 
+
+const char* ssid     = "myMobile";
+const char* password = "VollVergessen!DenScheiss!";
+const char* host = "192.168.43.1";
 
 void setup()
 {
+    Serial.begin(115200);
+    delay(10);
 
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop()
 {
+    delay(15000);
+
+    // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+    const int httpPort = 17580;
+    if (!client.connect(host, httpPort)) {
+        Serial.println("connection failed");
+        return;
+    }
+
+    // We now create a URI for the request
+    String url = "/pebble";
+    //url += streamId;
+    //url += "?private_key=";
+    //url += privateKey;
+    //url += "&value=";
+    //url += value;
+
+    Serial.print("Requesting URL: ");
+    Serial.println(url);
+
+    // This will send the request to the server
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            return;
+        }
+    }
     
+    // Read all the lines of the reply from server and print them to Serial
+    while(client.available()) {
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+    }
+
+    Serial.println();
+    Serial.println("closing connection");
 }
